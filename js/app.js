a811c79934b3f0c1276f179924eb8f90d6564663
Vladimir Nikishin <vladimir.nikishin@informicus.ru>
2018-12-25 16:06:56 +0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,53 @@
-function createTable() {
+var data = [];
+
+
+function loadData() {
+
+    $('#box').html("");
+    createTable(data.length/3  ,3);
+
+	for(let i = 0; i < data.length; i++) {
+        document.getElementById('cell_' + i)
+			.value = createTableElement(data[i]);
+	}
+};
+
+function saveComment() {
+
+	data.push({
+		name: $('#iName').val(),
+		email: $('#iMail').val(),
+		comment: $('#iComment').val(),
+	});
+
+	console.log(data);
+    clearInputFields();
+	loadData();
+};
+
+function clearInputFields() {
+    $('#iName').val("");
+    $('#iMail').val("");
+    $('#iComment').val("");
+};
+
+
+
+function createTable(rows, cols) {
 	 
 	divTable = $('<div></div>').attr({ class: "table-scroll" });
 	mytable = $('<table></table>').attr({ id: "basicTable" , class: "table table-hover"});
 	mytable.appendTo(divTable);
-	var rows = new Number($("#rowcount").val());
-	var cols = new Number($("#columncount").val());
+	let cellId = 0;
 	var tr = [];
 	for (var i = 0; i < rows; i++) {
 		var row = $('<tr></tr>').attr({ class: ["class1", "class2", "class3"].join(' ') }).appendTo(mytable);
 		for (var j = 0; j < cols; j++) {
-			col1 = $('<td></td>');
-			col1.html(createTableElement());
+			col1 = $('<td></td>').attr({ id: "cell_" + cellId });
+            cellId++;
 			col1.appendTo(row);
-
 		}
-		 		 
 	}
-	
 	console.log("TTTTT:"+divTable.html());
 	divTable.appendTo("#box");	       
   
@@ -26,11 +57,11 @@ function writeComment() {
 	console.log("Write");
 };
 
-function createTableElement() {
+function createTableElement(element, elStyle) {
 
-  tableElement ='<div class="card"><div class="card-header"><div class="row"><div class="col-lg-6"><label>' +$('#iName').val()+'</label></div></div></div><div class="card-body"><div class="row"><div class="col-lg-6"><label>'+$('#iMail').val()+'</label></div></div><div class="row"><div class="col-lg-6"><label>'+$('#iComment').val()+'</label></div></div></div></div>';
+    tableElement = '<div class="card"><div class="card-header"><div class="row"><div class="col-lg-6"><label>' + element.name + '</label></div></div></div>' +
+		'<div class="card-body"><div class="row"><div class="col-lg-6"><label>' + element.email + '</label></div></div>' +
+		'<div class="row"><div class="col-lg-6"><label>' + element.comment + '</label></div></div></div></div>';
 
-
-
-  return tableElement;
+    return tableElement;
 };
